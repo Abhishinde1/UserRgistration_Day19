359c89cd49a767aaaeb8fae1417da12823fc96bc
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-19 11:17:43 +0530

    Rule3 for PassWord program Added

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose option:\n1.Valid first name\n2.Valid last name\n3.Valid Email\n4.Valid Mobile number(+91 1234567890)\n5.Minimum 8 characters\n6.At least 1 upper case");
+            Console.WriteLine("Choose option:\n1.Valid first name\n2.Valid last name\n3.Valid Email\n4.Valid Mobile number(+91 1234567890)\n5.Minimum 8 characters\n6.At least 1 upper case\n7.At least 1 numberic value");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -30,6 +30,11 @@
                     Console.WriteLine("Enter a password : ");
                     string input2 = Console.ReadLine();
                     UC6.ValidatePassword(input2);
+                    break;
+                case 7:
+                    Console.WriteLine("Enter a password : ");
+                    string input3 = Console.ReadLine();
+                    UC7.ValidatePassword(input3);
                     break;
 
             }
