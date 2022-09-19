9f47928fcc77ec8240cfdcfccf628dcfdc3f22f1
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-19 10:45:32 +0530

    Valid First Name

---
+++ Day19/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose option:\n1.Valid first name");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    UC1.ValidFirstName();
+                    break;
+            }
+        }
+    }
+}
